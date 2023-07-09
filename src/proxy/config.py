1e4b6471c5e1aec37511600ad0ec84a1d0a4e3ce
Maximos Nikiforakis <nikiforos@live.co.uk>
2023-07-09 14:42:25 +0300

    proxy dockerfile

--- src/proxy/config.py
+++ src/proxy/config.py
@@ -3,12 +3,15 @@ import os
 
 from pydantic import BaseModel
 
+from dotenv import load_dotenv
+
 
 class Config(BaseModel):
     api_port: Optional[int] = None
     proxy_port: Optional[int] = None
 
     def init(self) -> None:
+        load_dotenv()
         self.api_port = int(os.getenv("API_PORT"))
         self.proxy_port = int(os.getenv("PROXY_PORT"))
 
